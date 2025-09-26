6b977de772ff11237ad65d61b3369cdec3fc054e
victorwhats <vmartinez@whatsbot.com.mx>
2025-09-25 23:37:31 -0600

    "Agregado flujo de trabajo"

--- Calculadora.java
+++ Calculadora.java
@@ -1,9 +1,12 @@
+
 public class Calculadora {
-public static void main(String[] args) {
-int resultado = sumar(5, 3);
-System.out.println("Resultado: " + resultado);
-}
-public static int sumar(int a, int b) {
-return a + b;
-}
+
+    public static void main(String[] args) {
+        int resultado = sumar(5, 3);
+        System.out.println("Resultado: " + resultado);
+    }
+
+    public static int sumar(int a, int b) {
+        return a + b;
+    }
 }
---
+++ WORKFLOW.md
@@ -0,0 +1,6 @@
+# Flujo de trabajo del proyecto
+a. Crear una nueva rama para cada función nueva.
+b. Programar la función en esa rama.
+c. Probar el código localmente.
+d. Hacer pull request para revisión.
+e. Fusionar a la rama principal (main) si todo está correcto.
