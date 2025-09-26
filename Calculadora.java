f250028ab524365e5144ad9899af6d7ff3658327
victorwhats <vmartinez@whatsbot.com.mx>
2025-09-25 23:34:07 -0600

    Primera carga del código base

---
+++ Calculadora.java
@@ -0,0 +1,9 @@
+public class Calculadora {
+public static void main(String[] args) {
+int resultado = sumar(5, 3);
+System.out.println("Resultado: " + resultado);
+}
+public static int sumar(int a, int b) {
+return a + b;
+}
+}
